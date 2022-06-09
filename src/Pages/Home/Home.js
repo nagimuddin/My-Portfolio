d6bc3d3321bf26b828ad7c0d2a6bc280a0f9152e
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-06-09 13:38:20 +0600

    header section updated

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Bennar from './Bennar';
 
 const Home = () => {
     return (
         <div>
-            
+            <Bennar></Bennar>
         </div>
     );
 };
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -28,32 +28,16 @@ const Header = () => {
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <Link to="/">Item 1</Link>
-              </li>
-              <li tabIndex="0">
-                <Link to="/" className="justify-between">
-                  Parent
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </Link>
-                <ul className="p-2">
-                  <li>
-                    <Link to="/">Submenu 1</Link>
-                  </li>
-                  <li>
-                    <Link to="/">Submenu 2</Link>
-                  </li>
-                </ul>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/">Item 3</Link>
+                <Link to="/">About Me</Link>
+              </li>
+              <li>
+                <Link to="/">Projects</Link>
+              </li>
+              <li>
+                <Link to="/">Blogs</Link>
               </li>
             </ul>
           </div>
@@ -66,42 +50,21 @@ const Header = () => {
             </div>
           </div>{" "}
         </div>
-        <div className="navbar-center hidden lg:flex">
+        <div className="navbar-end hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
-            <li>
-              <Link to="/">Item 1</Link>
-            </li>
-            <li tabIndex="0">
-              <Link to="/">
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </Link>
-              <ul className="p-2">
-                <li>
-                  <Link to="/">Submenu 1</Link>
-                </li>
-                <li>
-                  <Link to="/">Submenu 2</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link to="/">Item 3</Link>
-            </li>
+          <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/">About Me</Link>
+              </li>
+              <li>
+                <Link to="/">Projects</Link>
+              </li>
+              <li>
+                <Link to="/">Blogs</Link>
+              </li>
           </ul>
-        </div>
-        <div className="navbar-end">
-          <Link to="/" className="btn">
-            Get started
-          </Link>
         </div>
       </div>
     </>
