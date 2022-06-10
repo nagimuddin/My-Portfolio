88cf30975b953e89c3f1fdc726651ebae76562da
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-06-10 23:39:29 +0600

    footer section updated

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,12 +3,14 @@ import AboutMe from './AboutMe';
 import Available from './Available';
 import Projects from './Projects';
 import Cetagory from './Cetagory';
+import Skills from './Skills';
 
 const Home = () => {
     return (
         <div>
             <AboutMe></AboutMe>
             <Projects></Projects>
+            <Skills></Skills>
             <Cetagory></Cetagory>
             <Available></Available>
             
---
+++ src/Pages/Home/Skills.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <>
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content">
+          <div class="text-center lg:text-left">
+            <h2 class="py-3">My Skills</h2>
+            <h2 class="text-2xl font-bold">Every day <br/>I develop my skills</h2>
+            <p className="text-xl">I am a active learner.I always try to learn new in every day. My goal is want to be a full stack developer. Till now I gethard some experience these are.</p>
+          </div>
+          <div class="w-full grid grid-cols-3 gap-3 bg-base-100 p-8 bg-[#f5655a] mx-auto">
+              <div class="form-control mt-6">
+                <button class="btn btn-outline">Html</button>
+              </div>
+              <div class="form-control mt-6">
+                <button class="btn btn-outline">Css3</button>
+              </div>
+              <div class="form-control mt-6">
+                <button class="btn btn-outline">JavaScript</button>
+              </div>
+              <div class="form-control mt-6">
+                <button class="btn btn-outline">Bootstrape</button>
+              </div>
+              <div class="form-control mt-6">
+                <button class="btn btn-outline">react.js</button>
+              </div>
+              <div class="form-control mt-6">
+                <button class="btn btn-outline">Tailwind</button>
+              </div>
+              <div class="form-control mt-6">
+                <button class="btn btn-outline">Node.js</button>
+              </div>
+              <div class="form-control mt-6">
+                <button class="btn btn-outline">Firebase</button>
+              </div>
+              <div class="form-control mt-6">
+                <button class="btn btn-outline">Mongodb</button>
+              </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Skills;
